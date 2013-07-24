1799dcef477b13eadb39e0b8e87fd273eae987bc
Masahiro Taniuchi <mtaniuchi+github@gmail.com>
2013-07-24 22:47:32 +0900

    comment added

--- ExceptionAspectTest/ExceptionAspectTest/Program.cs
+++ ExceptionAspectTest/ExceptionAspectTest/Program.cs
@@ -1,4 +1,29 @@
-﻿using System;
+﻿/*
+ * The MIT License (MIT)
+ * 
+ * Copyright (c) 2013 Masahiro Taniuchi
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * 
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -113,7 +138,7 @@ namespace ExceptionAspectTest
             throw new ApplicationException("ChildMethod1 Exception");
 
             // FlowBehavior.Continueにしても、ここは実行されない
-            System.Diagnostics.Debug.WriteLine("ChildMethod1 continue...");
+            //System.Diagnostics.Debug.WriteLine("ChildMethod1 continue...");
         }
 
         //ここにアスペクトを適用するのが正しい
@@ -123,7 +148,7 @@ namespace ExceptionAspectTest
             throw new ApplicationException("ChildMethod1 Exception");
 
             // FlowBehavior.Continueにしても、ここは実行されない
-            System.Diagnostics.Debug.WriteLine("ChildMethod2 continue...");
+            //System.Diagnostics.Debug.WriteLine("ChildMethod2 continue...");
         }
 
         #endregion 多段ありタイプの実装例
@@ -183,7 +208,7 @@ namespace ExceptionAspectTest
             throw new ApplicationException("AnotherChildMethod2 Exception");
 
             // FlowBehavior.Continueにしても、ここは実行されない
-            System.Diagnostics.Debug.WriteLine("ChildMethod2 continue...");
+            //System.Diagnostics.Debug.WriteLine("ChildMethod2 continue...");
         }
 
         //ここにアスペクトを適用するのが正しい 
